bc134fe4a577cf1fb463a2ce3df7e2d8df1a7cf1
Hoàng Nguyên Bách <hoangnguyenbach2002@gmail.com>
2023-05-01 11:30:15 +0700

    improve UI for app

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -16,138 +16,141 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return Scaffold(
-      drawer: CustomDrawer(),
-      body: SafeArea(
-          child: Column(
-        children: [
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.only(left: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Builder(builder: (context) {
-                    return IconButton(
-                        onPressed: () {
-                          Scaffold.of(context).openDrawer();
-                        },
-                        icon: Icon(Icons.menu));
-                  }),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text(
-                        "27\u00b0",
-                        style: TextStyle(
-                            fontSize: 40, fontWeight: FontWeight.bold),
-                      ),
-                      Container(
-                        width: 90,
-                        alignment: Alignment.centerLeft,
-                        padding: EdgeInsets.all(8),
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            boxShadow: [
-                              BoxShadow(
-                                  blurRadius: 10,
-                                  spreadRadius: 5,
-                                  color: Colors.black.withOpacity(.1))
-                            ],
-                            borderRadius: const BorderRadius.only(
-                                topLeft: Radius.circular(35),
-                                bottomLeft: Radius.circular(35))),
-                        child: Container(
-                          width: 40,
-                          height: 40,
-                          decoration: BoxDecoration(
-                            color: KMainText,
-                            shape: BoxShape.circle,
-                          ),
-                          child: Icon(
-                            Icons.add,
-                            size: 30,
-                            color: Colors.white,
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        "Hi Bach",
-                        style: TextStyle(
-                            fontSize: 30, fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        "Welcome to your home !",
-                        style: TextStyle(
-                            fontSize: 18,
-                            color: KMainText.withOpacity(.5),
-                            fontWeight: FontWeight.bold),
-                      )
-                    ],
-                  ),
-                  Container(
-                    width: double.infinity,
-                    height: 70,
-                    alignment: Alignment.centerLeft,
-                    padding: EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        boxShadow: [
-                          BoxShadow(
-                              blurRadius: 10,
-                              spreadRadius: 5,
-                              color: Colors.black.withOpacity(.1))
-                        ],
-                        borderRadius: const BorderRadius.only(
-                            topLeft: Radius.circular(35),
-                            bottomLeft: Radius.circular(35))),
-                    child: Row(
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        drawer: CustomDrawer(),
+        body: SafeArea(
+            child: Column(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Builder(builder: (context) {
+                      return IconButton(
+                          onPressed: () {
+                            Scaffold.of(context).openDrawer();
+                          },
+                          icon: Icon(Icons.menu));
+                    }),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        userAvatar(
-                            "https://randomuser.me/api/portraits/men/51.jpg"),
-                        userAvatar(
-                            "https://randomuser.me/api/portraits/women/13.jpg"),
-                        userAvatar(
-                            "https://randomuser.me/api/portraits/women/45.jpg"),
-                        userAvatar(
-                            "https://randomuser.me/api/portraits/women/87.jpg"),
-                        Container(
-                          width: 50,
-                          height: 50,
-                          decoration: BoxDecoration(
-                            color: KMainText.withOpacity(.1),
-                            shape: BoxShape.circle,
-                          ),
-                          child: Icon(
-                            Icons.add,
-                            size: 30,
-                            color: Colors.white,
-                          ),
+                        const Text(
+                          "27\u00b0",
+                          style: TextStyle(
+                              fontSize: 40, fontWeight: FontWeight.bold),
                         ),
+                        Container(
+                          width: 90,
+                          alignment: Alignment.centerLeft,
+                          padding: EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              boxShadow: [
+                                BoxShadow(
+                                    blurRadius: 10,
+                                    spreadRadius: 5,
+                                    color: Colors.black.withOpacity(.1))
+                              ],
+                              borderRadius: const BorderRadius.only(
+                                  topLeft: Radius.circular(35),
+                                  bottomLeft: Radius.circular(35))),
+                          child: Container(
+                            width: 40,
+                            height: 40,
+                            decoration: BoxDecoration(
+                              color: KMainText,
+                              shape: BoxShape.circle,
+                            ),
+                            child: Icon(
+                              Icons.add,
+                              size: 30,
+                              color: Colors.white,
+                            ),
+                          ),
+                        )
                       ],
                     ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      SizedBox(
-                        height: 20,
-                      )
-                    ],
-                  ),
-                ],
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          "Hi Bach",
+                          style: TextStyle(
+                              fontSize: 30, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "Welcome to your home !",
+                          style: TextStyle(
+                              fontSize: 18,
+                              color: KMainText.withOpacity(.5),
+                              fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                    Container(
+                      width: double.infinity,
+                      height: 70,
+                      alignment: Alignment.centerLeft,
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                                blurRadius: 10,
+                                spreadRadius: 5,
+                                color: Colors.black.withOpacity(.1))
+                          ],
+                          borderRadius: const BorderRadius.only(
+                              topLeft: Radius.circular(35),
+                              bottomLeft: Radius.circular(35))),
+                      child: Row(
+                        children: [
+                          userAvatar(
+                              "https://randomuser.me/api/portraits/men/51.jpg"),
+                          userAvatar(
+                              "https://randomuser.me/api/portraits/women/13.jpg"),
+                          userAvatar(
+                              "https://randomuser.me/api/portraits/women/45.jpg"),
+                          userAvatar(
+                              "https://randomuser.me/api/portraits/women/87.jpg"),
+                          Container(
+                            width: 50,
+                            height: 50,
+                            decoration: BoxDecoration(
+                              color: KMainText.withOpacity(.1),
+                              shape: BoxShape.circle,
+                            ),
+                            child: Icon(
+                              Icons.add,
+                              size: 30,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        SizedBox(
+                          height: 20,
+                        )
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-          Expanded(child: RoomCard(homeData: smartHome)),
-        ],
-      )),
+            Expanded(child: RoomCard(homeData: smartHome)),
+          ],
+        )),
+      ),
     );
   }
 
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -81,184 +81,190 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          width: size.width,
-          height: size.height,
-          child: Stack(
-            alignment: Alignment.center,
-            children: <Widget>[
-              Positioned(
-                  top: 0,
-                  left: 0,
-                  child: Image.asset(
-                    "assets/images/main_top.png",
-                    width: size.width * 0.35,
-                  )),
-              Positioned(
-                  bottom: 0,
-                  right: 0,
-                  child: Image.asset(
-                    "assets/images/login_bottom.png",
-                    width: size.width * 0.4,
-                  )),
-              Positioned(
-                top: 40,
-                left: size.width * 0.37,
-                child: Text('Sign In',
-                    style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
-                        color: KMainText)),
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Image.asset(
-                    "assets/images/ava2.jpg",
-                    width: size.width * 0.5,
-                    height: size.height * 0.3,
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Container(
-                    margin: EdgeInsets.symmetric(vertical: 10),
-                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                    width: size.width * 0.8,
-                    height: size.height / 11,
-                    decoration: BoxDecoration(
-                        color: kPrimaryLightColor,
-                        borderRadius: BorderRadius.circular(30)),
-                    child: TextField(
-                      controller: emailTextEditingController,
-                      onChanged: (value) {},
-                      decoration: InputDecoration(
-                          icon: Icon(
-                            Icons.person,
-                            color: KMainText,
-                          ),
-                          labelText: "Your Email",
-                          hintText: "abc@gmail.com",
-                          border: InputBorder.none),
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Container(
+            width: size.width,
+            height: size.height,
+            child: Stack(
+              alignment: Alignment.center,
+              children: <Widget>[
+                Positioned(
+                    top: 0,
+                    left: 0,
+                    child: Image.asset(
+                      "assets/images/main_top.png",
+                      width: size.width * 0.35,
+                    )),
+                Positioned(
+                    bottom: 0,
+                    right: 0,
+                    child: Image.asset(
+                      "assets/images/login_bottom.png",
+                      width: size.width * 0.4,
+                    )),
+                Positioned(
+                  top: 40,
+                  left: size.width * 0.37,
+                  child: Text('Sign In',
+                      style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          color: KMainText)),
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image.asset(
+                      "assets/images/ava2.jpg",
+                      width: size.width * 0.5,
+                      height: size.height * 0.3,
                     ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.symmetric(vertical: 10),
-                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                    width: size.width * 0.8,
-                    height: size.height / 11,
-                    decoration: BoxDecoration(
-                        color: kPrimaryLightColor,
-                        borderRadius: BorderRadius.circular(30)),
-                    child: TextField(
-                      controller: passwordTextEditingController,
-                      obscureText: passwordVisible,
-                      onChanged: (value) {},
-                      decoration: InputDecoration(
-                          icon: Icon(
-                            Icons.lock,
-                            color: KMainText,
-                          ),
-                          labelText: "Password",
-                          suffixIcon: IconButton(
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 10),
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                      width: size.width * 0.8,
+                      height: size.height / 11,
+                      decoration: BoxDecoration(
+                          color: kPrimaryLightColor,
+                          borderRadius: BorderRadius.circular(30)),
+                      child: TextField(
+                        controller: emailTextEditingController,
+                        onChanged: (value) {},
+                        decoration: InputDecoration(
                             icon: Icon(
-                              passwordVisible
-                                  ? Icons.visibility
-                                  : Icons.visibility_off,
+                              Icons.person,
                               color: KMainText,
                             ),
-                            onPressed: () {
-                              setState(
-                                () {
-                                  passwordVisible = !passwordVisible;
-                                },
-                              );
-                            },
-                          ),
-                          border: InputBorder.none),
-                    ),
-                  ),
-                  Row(
-                    children: [
-                      SizedBox(
-                        width: 220,
+                            labelText: "Your Email",
+                            hintText: "abc@gmail.com",
+                            border: InputBorder.none),
                       ),
-                      Container(
-                        padding: EdgeInsets.only(top: 15, left: 20),
-                        child: InkWell(
-                          child: Text(
-                            'Forgot Password',
-                            style: TextStyle(
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 10),
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                      width: size.width * 0.8,
+                      height: size.height / 11,
+                      decoration: BoxDecoration(
+                          color: kPrimaryLightColor,
+                          borderRadius: BorderRadius.circular(30)),
+                      child: TextField(
+                        controller: passwordTextEditingController,
+                        obscureText: passwordVisible,
+                        onChanged: (value) {},
+                        decoration: InputDecoration(
+                            icon: Icon(
+                              Icons.lock,
+                              color: KMainText,
+                            ),
+                            labelText: "Password",
+                            suffixIcon: IconButton(
+                              icon: Icon(
+                                passwordVisible
+                                    ? Icons.visibility
+                                    : Icons.visibility_off,
                                 color: KMainText,
-                                fontWeight: FontWeight.bold,
-                                decoration: TextDecoration.none),
-                          ),
-                          onTap: () {},
-                        ),
+                              ),
+                              onPressed: () {
+                                setState(
+                                  () {
+                                    passwordVisible = !passwordVisible;
+                                  },
+                                );
+                              },
+                            ),
+                            border: InputBorder.none),
                       ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Container(
-                    width: size.width * 0.8,
-                    height: size.height / 13,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(29),
-                      child: MaterialButton(
-                        padding:
-                            EdgeInsets.symmetric(vertical: 20, horizontal: 40),
-                        color: KMainText,
-                        onPressed: () {
-                          FirebaseAuth.instance
-                              .signInWithEmailAndPassword(
-                                  email: emailTextEditingController.text,
-                                  password: passwordTextEditingController.text)
-                              .then((value) {
-                            Navigator.pushNamed(context, "/home");
-                          }).onError((error, stackTrace) {
-                            print("Error ${error.toString()}");
-                          });
-                        },
-                        child: Text(
-                          "SIGN IN",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: size.width * 0.035),
+                    ),
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: 220,
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(top: 15, left: 20),
+                          child: InkWell(
+                            child: Text(
+                              'Forgot Password',
+                              style: TextStyle(
+                                  color: KMainText,
+                                  fontWeight: FontWeight.bold,
+                                  decoration: TextDecoration.none),
+                            ),
+                            onTap: () {},
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Container(
+                      width: size.width * 0.8,
+                      height: size.height / 13,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(29),
+                        child: MaterialButton(
+                          padding: EdgeInsets.symmetric(
+                              vertical: 20, horizontal: 40),
+                          color: KMainText,
+                          onPressed: () {
+                            FirebaseAuth.instance
+                                .signInWithEmailAndPassword(
+                                    email: emailTextEditingController.text,
+                                    password:
+                                        passwordTextEditingController.text)
+                                .then((value) {
+                              Navigator.pushNamed(context, "/home");
+                            }).onError((error, stackTrace) {
+                              print("Error ${error.toString()}");
+                            });
+                          },
+                          child: Text(
+                            "SIGN IN",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: size.width * 0.035),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        "Don't have an account ? ",
-                        style: TextStyle(color: kPrimaryLightColor),
-                      ),
-                      InkWell(
-                        child: Text(
-                          'Sign up',
-                          style: TextStyle(color: KMainText),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          "Don't have an account ? ",
+                          style: TextStyle(color: kPrimaryLightColor),
                         ),
-                        onTap: () {
-                          Navigator.push(context,
-                              MaterialPageRoute(builder: (context) {
-                            return SignupScreen();
-                          }));
-                        },
-                      )
-                    ],
-                  ),
-                ],
-              ),
-            ],
+                        InkWell(
+                          child: Text(
+                            'Sign up',
+                            style: TextStyle(color: KMainText),
+                          ),
+                          onTap: () {
+                            Navigator.push(context,
+                                MaterialPageRoute(builder: (context) {
+                              return SignupScreen();
+                            }));
+                          },
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
